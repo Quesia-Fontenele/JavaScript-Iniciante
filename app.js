177ac4cafa0c6382bdb89d27eb0a8d780166d3e8
Kesia <quesiafontenele@yahoo.com>
2025-02-24 21:58:14 -0300

    javascript-iniciante

---
+++ app.js
@@ -0,0 +1,134 @@
+// let palavra = "Hello world"
+// console.log(palavra)
+// palavra = "Testando mudança de valor"
+// console.log(palavra)
+
+// let palavra = "String" //texto
+// let numero = 10
+// let bool = true
+// let nulo = null
+// let batata = //underfined
+// let lista = []
+// let obj = {}
+
+// let nome = "Elias"
+// let idade = 19
+// console.log (`Bem vindo,${nome}. Você tem ${idade} anos de idade`)
+
+// let saudacao = "Olá,"
+// let nomepessoa = "Kesia"
+// console.log(`${saudacao} ${nomepessoa}`)
+
+// let eu = "Eu"
+// let adoro = "adoro"
+// let javascript = "JavaScript"
+// console.log(`${eu} ${adoro} ${javascript}`)
+
+// let nome = "Patricia"
+// let matricula = 432341
+// let escola = "Colégio Farias Brito"
+// console.log(`Olá, ${nome}. Sua matricula é ${matricula}. ${escola}`)
+
+// let convidado = "Quesia"
+// let evento = "Casamento da Patricia Poeta"
+// let local = "Buffet Lulla's"
+// console.log(`${convidado}, parabéns. Você esta convidada para o grande evento, 
+//     o ${evento}. O evento irá acontecer no ${local}, contamos com a sua presença`)
+
+// let lista = [123,23,423,5466]
+// let lista2 = ["oi","som"]
+// let lista3 = [true,false,true,false]
+// let lista4 = ["oi",null,undefined,23,"teste"]
+
+// console.log(lista3[3])
+// console.log(lista[2])
+// console.log(lista4[1])
+// console.log(lista2[1])
+
+// let filme = {
+//     id: 1,
+//     titulo: "O senhor dos aneis: A sociedade do anel",
+//     ano: 2001,
+//     genero: "Fantasia"
+// }
+// console.log(`O filme ${filme.titulo} do ano ${filme.ano} é o meu filme favorito`)
+
+// let estudante = {
+//     nome: "marcao o cara da corrida"
+//     idade: 30,
+//     curso: "corrida ao ar livre"
+// }
+// console.log(`meu nome é ${estudante.nome}, tenho $ {estudante.idade} anos de idade estou cursando ${estudando.curso}`)
+// let objeto = {
+//     nome: "azeite",
+//     preco: 42,
+//     desconto: 0.2
+// }
+// let valorFinal = objeto.preco * (1-objeto.desconto)
+
+// console.log(`o preco original do item ${objeto.nome} é ${objeto.preco}. Com desconto de ${objeto.desconto} o preço final será de ${valorFinal}`);
+// let endereco = {
+//     rua: "Rua menino jesus 503 A",
+//     cidade: "Fortaleza",
+//     estado: "Ceará"
+// }
+// console.log(`Eu moro na rua ${endereco.rua}, na cidade de ${endereco.cidade}, estado de ${endereco.estado}`)
+
+// let produto = {
+//     nome: "Camiseta",
+//     preco: 158,
+//     quantidade: 7
+// }
+// console.log(o total da sua compra é ${produto.preco} * ${produto.quantidade})
+
+let usuarios = [
+    {
+        id:1,
+        nome:"Zezim",
+        cpf:89742386324,
+        telefone:324234324
+    },
+    {
+        id:2,
+        nome:"Elias",
+        cpf:8974233232,
+        telefone:12342123
+    },
+    {
+        id:3,
+        nome:"Junim",
+        cpf:48329747786,
+        telefone:43242232
+    }
+]
+// console.log(${usuarios[index].nome})
+
+// console.log(`${usuarios[2].nome} de ${usuarios[2].cpf} está com a matricula travada, favor ligar para ${usuarios[2].telefone}`)
+
+// console.log(`a oficina do ${usuarios[0].nome} esta aberta hoje, ligue para ${usuarios[0].telefone} para mais informações`)
+// console.log(`o ${usuarios[1].nome} de ${usuarios[0].cpf} foi sorteado no totolec, por favor venha retirar seu premio`)
+
+// let soma = 10 + 20
+// let sub = 20 - 10
+// let mult = 2 * 20
+// let divisao = 10/2
+// let exp = 2**3
+// let raiz = 25**(1/2)
+// console.log("soma:",soma)
+// console.log("Subtração:",sub)
+// console.log("multiplicação:", mult)
+// console.log("divisao:", divisao)
+// console.log("exponencial:", exp)
+// console.log("raiz:", raiz)
+
+// let numero1 = prompt("Digite o primeiro número: ")
+// let numero2 = prompt("Digite o segundo número: ")
+
+// let soma = Number(numero1) + Number(numero2)
+// alert (soma)
+
+let nome = prompt("Digite seu nome: ")
+
+alert(`Bem vinde, ${nome}`)
+
+
